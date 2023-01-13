fd259b094a8b636fc4dacd1f8d631c9535824ab4
galaxy zhang <694553456@qq.com>
2023-01-13 21:14:52 +0800

    add: 添加tag更多功能

--- src/modules/articles/tag/tagController.js
+++ src/modules/articles/tag/tagController.js
@@ -1,7 +1,10 @@
 const tagRouter = require('koa-router')();
 const tagService = require('./tagService');
 
-tagRouter.post('/tag', tagService.createTag);
+tagRouter.post('/createTag', tagService.createTag);
+tagRouter.get('/getAllTags', tagService.getAllTags);
+tagRouter.get('/modifyTag', tagService.modifyTag);
+tagRouter.get('/deleteTag', tagService.deleteTag);
 tagRouter.get('/test', tagService.test);
 
 module.exports = tagRouter;
